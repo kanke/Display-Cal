9de3dd2b87200efbb04b9bcfe12e5142b8ec9421
Jon Bevan <jbevan@adaptavist.com>
2021-11-15 13:26:25 +0000

    Output as CSV with pagination

--- src/main/java/CalendarQuickstart.java
+++ src/main/java/CalendarQuickstart.java
@@ -14,6 +14,7 @@ import com.google.api.services.calendar.CalendarScopes;
 import com.google.api.services.calendar.model.Event;
 import com.google.api.services.calendar.model.Events;
 
+import javax.annotation.Nullable;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
@@ -33,6 +34,10 @@ public class CalendarQuickstart {
      */
     private static final List<String> SCOPES = Collections.singletonList(CalendarScopes.CALENDAR_READONLY);
     private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
+
+    private static final DateTime yearAgo = new DateTime(System.currentTimeMillis() - (365L * 24 * 60 * 60 * 1000));
+    private static final DateTime now = new DateTime(System.currentTimeMillis());
+
 
     /**
      * Creates an authorized Credential object.
@@ -59,6 +64,65 @@ public class CalendarQuickstart {
         return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
     }
 
+    private static String escapeNewlines(String input) {
+        return input.replaceAll("\n", "\\\\\\n").replaceAll("\r", "");
+    }
+
+    private static String quoteIfHasComma(@Nullable String input) {
+        if (input == null) {
+            return "";
+        }
+        if (input.contains(",")) {
+            return "\"" + escapeNewlines(input.replaceAll("\"", "\\\\\"")) + "\"";
+        }
+        return escapeNewlines(input);
+    }
+
+    private static void formatRow(Event event) {
+        System.out.printf(
+                "%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s%n",
+                quoteIfHasComma(event.getId()),
+                quoteIfHasComma(event.getOrganizer().getDisplayName()),
+                quoteIfHasComma(event.getOrganizer().getEmail()),
+                quoteIfHasComma(event.getSummary()),
+                quoteIfHasComma(event.getDescription()),
+                quoteIfHasComma(event.getStatus()),
+                event.getStart().getDateTime(),
+                event.getEnd().getDateTime(),
+                quoteIfHasComma(event.getAttendees() == null || event.getAttendees().isEmpty() ? "" : event.getAttendees().toString()),
+                event.getAttendeesOmitted() != null && event.getAttendeesOmitted() ? "yes" : "no",
+                quoteIfHasComma(event.getConferenceData() != null ? event.getConferenceData().toString() : ""),
+                quoteIfHasComma(event.getHangoutLink()),
+                quoteIfHasComma(event.getICalUID()),
+                quoteIfHasComma(event.getLocation()),
+                quoteIfHasComma(event.getRecurringEventId())
+        );
+    }
+
+    private static void fetchEvents(Calendar service, String id, @Nullable String pageToken) throws IOException {
+        final Calendar.Events.List request = service.events()
+                .list(id)
+                .setTimeMin(yearAgo)
+                .setTimeMax(now)
+                .setOrderBy("startTime")
+                .setSingleEvents(true);
+        if (pageToken != null) {
+            request.setPageToken(pageToken);
+        }
+
+        Events events = request.execute();
+        List<Event> items = events.getItems();
+        if (!items.isEmpty()) {
+            for (Event event : items) {
+                formatRow(event);
+            }
+        }
+
+        if (events.getNextPageToken() != null) {
+            fetchEvents(service, id, events.getNextPageToken());
+        }
+    }
+
     public static void main(String... args) throws IOException, GeneralSecurityException {
         // Build a new authorized API client service.
         final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
@@ -66,50 +130,21 @@ public class CalendarQuickstart {
                 .setApplicationName(APPLICATION_NAME)
                 .build();
 
-        // List the next 10 events from the primary calendar.
-        DateTime now = new DateTime(System.currentTimeMillis());
-        Events events = service.events().list("primary")
-                .setMaxResults(10)
-                .setTimeMin(now)
-                .setOrderBy("startTime")
-                .setSingleEvents(true)
-                .execute();
-        List<Event> items = events.getItems();
-        if (items.isEmpty()) {
-            System.out.println("No upcoming events found.");
-        } else {
-            System.out.println("UPCOMING EVENTS");
-            System.out.println("\n");
-            for (Event event : items) {
-                DateTime start = event.getStart().getDateTime();
-                DateTime end = event.getEnd().getDateTime();
-                if (start == null || end == null) {
-                    start = event.getStart().getDate();
-                    end = event.getEnd().getDate();
-                }
-                // System.out.printf("%s (%s) * \n", event.getAttendees(), event.getDescription(), event.getAttendeesOmitted(), event.getColorId(), event.getConferenceData(), event.getSummary(), start);
-                System.out.println("*************************************");
-                System.out.println("Summary " + event.getSummary());
-                System.out.println("Status " + event.getStatus());
-                System.out.println("StartTime " + start);
-                System.out.println("EndTime " + end);
-                System.out.println("Id " + event.getId());
-                System.out.println("Attendees " + event.getAttendees());
-                System.out.println("Description " + event.getDescription());
-                System.out.println("AttendeesOmitted " + event.getAttendeesOmitted());
-                System.out.println("ColorId " + event.getColorId());
-                System.out.println("ConferenceData " + event.getConferenceData());
-                System.out.println("HangoutLink " + event.getHangoutLink());
-                System.out.println("HtmlLink " + event.getHtmlLink());
-                System.out.println("ICalUID " + event.getICalUID());
-                System.out.println("Location " + event.getLocation());
-                System.out.println("RecurringEventId " + event.getRecurringEventId());
-                System.out.println("Transparency " + event.getTransparency());
-                System.out.println("ConferenceData " + event.getConferenceData());
-                System.out.println("EndTimeUnspecified " + event.getEndTimeUnspecified());
-                System.out.println("Organizer " + event.getOrganizer());
-                System.out.println("\n");
-            }
-        }
+        System.out.println("ID," +
+                "Organiser Name," +
+                "Organiser Email," +
+                "Summary," +
+                "Description," +
+                "Status," +
+                "Start DateTime," +
+                "End DateTime," +
+                "Attendees," +
+                "Attendees Omitted," +
+                "Conference Data," +
+                "Hangout Link," +
+                "ICalUid," +
+                "Location," +
+                "Recurring EventId");
+        fetchEvents(service, "primary", null);
     }
 }
